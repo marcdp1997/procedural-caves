1b5dab12ca4cc4e98444e6426b36ab73fc686ecb
Marc de Pedro <marc.depedro@univr.se>
2024-04-30 22:08:08 +0200

    Voxels list decides if cube is created or not

--- procedural-caves/Assets/Scripts/Cube.cs
+++ procedural-caves/Assets/Scripts/Cube.cs
@@ -6,29 +6,51 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     [SerializeField] private float _cubeSize;
-    [SerializeField] private Vector3 _worldSize;
+    [SerializeField] private Vector3Int _worldSize;
 
     private Mesh _mesh;
     private int _lastVertex;
     private MeshFilter _meshFilter;
     private List<Vector3> _vertices = new List<Vector3>();
     private List<int> _triangles = new List<int>();
-    private List<Vector2> _uvs = new List<Vector2>();
+    private byte[,,] _voxels;
+
+    private Vector3 Forward { get { return Vector3.forward; } }
+    private Vector3 Back { get { return Vector3.back; } }
+    private Vector3 Up { get { return Vector3.up; } }
+    private Vector3 Right { get { return Vector3.right; } }
+    private Vector3 Left { get { return Vector3.left; } }
 
     private void Start()
     {
-        CreateWorld();
+        PopulateVoxels();
+        CreateVoxels();
 
         _mesh = new Mesh();
         _mesh.vertices = _vertices.ToArray();
         _mesh.triangles = _triangles.ToArray();
-        _mesh.SetUVs(0, _uvs);
 
         _meshFilter = GetComponent<MeshFilter>();
         _meshFilter.mesh = _mesh;
     }
 
-    private void CreateWorld()
+    private void PopulateVoxels()
+    {
+        _voxels = new byte[_worldSize.x, _worldSize.y, _worldSize.z];
+
+        for (int x = 0; x < _worldSize.x; x++)
+        {
+            for (int y = 0; y < _worldSize.y; y++)
+            {
+                for (int z = 0; z < _worldSize.z; z++)
+                {
+                    _voxels[x, y, z] = Random.Range(0, 100) > 50 ? (byte)1 : (byte)0;
+                }
+            }
+        }
+    }
+
+    private void CreateVoxels()
     {
         for (int x = 0; x < _worldSize.x; x++)
         {
@@ -36,33 +58,31 @@ public class Cube : MonoBehaviour
             {
                 for (int z = 0; z < _worldSize.z; z++)
                 {
-                    CreateCube(new Vector3(x - _worldSize.x * 0.5f, y - _worldSize.y * 0.5f, z - _worldSize.z * 0.5f));
+                    if (_voxels[x, y, z] == 1)
+                        CreateCube(new Vector3(x, y, z));
                 }
             }
         }
     }
 
-    private void CreateCube(Vector3 worldPosition)
+    private void CreateCube(Vector3 coordinates)
     {
-        Vector3 start = worldPosition * _cubeSize;
-        Vector3 forward = Vector3.forward * _cubeSize;
-        Vector3 back = Vector3.back * _cubeSize;
-        Vector3 up = Vector3.up * _cubeSize;
-        Vector3 right = Vector3.right * _cubeSize;
-        Vector3 left = Vector3.left * _cubeSize;
-
-        GenerateFace(start, right, up);
-        GenerateFace(start + right, forward, up);
-        GenerateFace(start + forward, back, up);
-        GenerateFace(start + forward + right, left, up);
-        GenerateFace(start + up, right, forward);
-        GenerateFace(start + forward, right, back);
+        GenerateFace(coordinates, Right, Up);
+        GenerateFace(coordinates + Right, Forward, Up);
+        GenerateFace(coordinates + Forward, Back, Up);
+        GenerateFace(coordinates + Forward + Right, Left, Up);
+        GenerateFace(coordinates + Up, Right, Forward);
+        GenerateFace(coordinates + Forward, Right, Back);
     }
 
     // 2 3 | Clock-wise to 
     // 0 1 | define normals
     private void GenerateFace(Vector3 startPos, Vector3 right, Vector3 up)
     {
+        startPos = (startPos - ((Vector3)_worldSize * 0.5f)) * _cubeSize;
+        right *= _cubeSize;
+        up *= _cubeSize;
+
         _lastVertex = _vertices.Count;
 
         _vertices.Add(startPos); //0
