40c08fe7ec086fda65c37c6aca1d743adccae6b8
Marc de Pedro <marc.depedro@univr.se>
2024-05-04 12:34:33 +0200

    Rooms with different sizes

--- Assets/Scripts/WorldManager.cs
+++ Assets/Scripts/WorldManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class WorldManager : MonoBehaviour
@@ -12,7 +13,7 @@ public class WorldManager : MonoBehaviour
 
     [Header("Rooms")]
     [SerializeField] private float _numRooms;
-    [SerializeField] private float _roomSize;
+    [SerializeField] private List<float> _roomSizes;
 
     private byte[,,] _voxels;
 
@@ -20,6 +21,7 @@ public class WorldManager : MonoBehaviour
     {
         PopulateVoxels();
         CreateCenterCylinder();
+        CreateRooms();
         new WorldCreator(_voxels, _cubeSize, _cubeMaterial);
     }
 
@@ -42,20 +44,18 @@ public class WorldManager : MonoBehaviour
     private void CreateCenterCylinder()
     {
         int cylinderCenterX = _worldSize.x / 2;
-        int cylinderCenterY = _worldSize.y / 2;
-        int cylinderTopZ = (_worldSize.z - _worldSize.y) / 2 + _worldSize.y;
+        int cylinderTopY = _worldSize.y;
+        int cylinderCenterZ = _worldSize.z / 2;
 
-        for (int x = 0; x < _worldSize.x; x++)
+        for (int x = 0; x < _worldSize.y; x++)
         {
             for (int y = 0; y < _worldSize.y; y++)
             {
                 for (int z = 0; z < _worldSize.z; z++)
                 {
-                    int distanceSquared = (x - cylinderCenterX) * (x - cylinderCenterX) +
-                                          (y - cylinderCenterY) * (y - cylinderCenterY);
+                    int distanceSquared = (int)(Mathf.Pow(x - cylinderCenterX, 2) + Mathf.Pow(z - cylinderCenterZ, 2));
 
-                    if (distanceSquared <= _centerRadius * _centerRadius &&
-                        z >= (_worldSize.z - _worldSize.y) / 2 && z < cylinderTopZ)
+                    if (distanceSquared <= Mathf.Pow(_centerRadius, 2) && y < cylinderTopY)
                         _voxels[x, y, z] = 0;
                 }
             }
@@ -66,7 +66,32 @@ public class WorldManager : MonoBehaviour
     {
         for (int i = 0; i < _numRooms; i++)
         {
+            int roomSizeIndex = Random.Range(0, _roomSizes.Count);
+            int roomHalfSize = (int)(_roomSizes[roomSizeIndex] * 0.5f);
 
+            int x = Random.Range(roomHalfSize, _worldSize.x - roomHalfSize);
+            int y = Random.Range(roomHalfSize, _worldSize.y - roomHalfSize);
+            int z = Random.Range(roomHalfSize, _worldSize.z - roomHalfSize);
+            CreateRoom(new Vector3Int(x, y, z), _roomSizes[roomSizeIndex]);
+        }
+    }
+
+    private void CreateRoom(Vector3Int position, float roomSize)
+    {
+        int roomHalfSize = (int)(roomSize * 0.5f);
+        int initPosX = position.x - roomHalfSize;
+        int initPosY = position.y - roomHalfSize;
+        int initPosZ = position.z - roomHalfSize;
+
+        for (int x = initPosX; x < initPosX + roomSize; x++)
+        {
+            for (int y = initPosY; y < initPosY + roomSize; y++)
+            {
+                for (int z = initPosZ; z < initPosZ + roomSize; z++)
+                {
+                     _voxels[x, y, z] = 0;
+                }
+            }
         }
     }
 }
